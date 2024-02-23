0257d59102082fabf7c5484a21c3c7259ce31231
Jake Trent <jake.trent@angel.com>
2024-02-23 15:45:43 -0700

    mod11: navigate, patch, change pg title, handle_params

--- lib/live_view_studio_web/live/servers_live.ex
+++ lib/live_view_studio_web/live/servers_live.ex
@@ -4,31 +4,61 @@ defmodule LiveViewStudioWeb.ServersLive do
   alias LiveViewStudio.Servers
 
   def mount(_params, _session, socket) do
+    IO.inspect(self(), label: "mount")
     servers = Servers.list_servers()
 
     socket =
       assign(socket,
         servers: servers,
-        selected_server: hd(servers),
         coffees: 0
       )
 
     {:ok, socket}
   end
 
+  # invoked after mount
+  # and also after patch request
+  def handle_params(%{"id" => id}, _uri, socket) do
+    IO.inspect(self(), label: "HANDLE PARAMS id=#{id}")
+    server = Servers.get_server!(id)
+
+    {:noreply,
+     assign(socket,
+       selected_server: server,
+       page_title: "What's up #{server.name}?"
+     )}
+  end
+
+  def handle_params(_params, _uri, socket) do
+    IO.inspect(self(), label: "HANDLE PARAMS catcha all")
+
+    {:noreply,
+     assign(socket,
+       selected_server: hd(socket.assigns.servers)
+     )}
+  end
+
+  # ~p is a sigil for path interpolation - verifies that it's in the router
+  # query collection as query params
+  # server uses .id by default
+  # #patch keeps same liveview process; href is intercepted
+  # #navigate is a redirect, but in same layout, also intercepted; when most useful?
   def render(assigns) do
+    IO.inspect(self(), label: "RENDER")
+
     ~H"""
     <h1>Servers</h1>
     <div id="servers">
       <div class="sidebar">
         <div class="nav">
-          <a
+          <.link
             :for={server <- @servers}
+            patch={~p"/servers?#{[id: server]}"}
             class={if server == @selected_server, do: "selected"}
           >
             <span class={server.status}></span>
             <%= server.name %>
-          </a>
+          </.link>
         </div>
         <div class="coffees">
           <button phx-click="drink">
@@ -64,7 +94,11 @@ defmodule LiveViewStudioWeb.ServersLive do
               </blockquote>
             </div>
           </div>
-          <div class="links"></div>
+          <div class="links">
+            <.link navigate={~p"/light"}>
+              Adjust lights
+            </.link>
+          </div>
         </div>
       </div>
     </div>
@@ -72,6 +106,7 @@ defmodule LiveViewStudioWeb.ServersLive do
   end
 
   def handle_event("drink", _, socket) do
+    IO.inspect(self(), label: "handle evt")
     {:noreply, update(socket, :coffees, &(&1 + 1))}
   end
 end
