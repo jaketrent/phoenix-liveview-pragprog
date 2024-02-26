ef2d07148732b79d17920ba6b09801248e16b6b3
Jake Trent <jake.trent@angel.com>
2024-02-26 16:56:25 -0700

    mod23: js command for toggling classes

--- lib/live_view_studio_web/live/shop_live.ex
+++ lib/live_view_studio_web/live/shop_live.ex
@@ -2,6 +2,7 @@ defmodule LiveViewStudioWeb.ShopLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Products
+  alias Phoenix.LiveView.JS
 
   def mount(_params, _session, socket) do
     {:ok,
@@ -12,13 +13,12 @@ defmodule LiveViewStudioWeb.ShopLive do
      )}
   end
 
-  def handle_event("toggle-show-cart", _, socket) do
-    socket = update(socket, :show_cart, fn show -> !show end)
-    {:noreply, socket}
-  end
-
   def handle_event("add-product", %{"product" => product}, socket) do
     cart = Map.update(socket.assigns.cart, product, 1, &(&1 + 1))
     {:noreply, assign(socket, :cart, cart)}
+  end
+
+  def toggle_cart do
+    JS.toggle(to: "#cart") |> JS.toggle(to: "#backdrop")
   end
 end
