af619ad89a63e546ed8da976b02a9066363fcdcb
Jake Trent <jake.trent@angel.com>
2024-02-26 16:44:08 -0700

    mod22: presence tracking

--- lib/live_view_studio_web/live/presence_live.ex
+++ lib/live_view_studio_web/live/presence_live.ex
@@ -1,20 +1,53 @@
 defmodule LiveViewStudioWeb.PresenceLive do
   use LiveViewStudioWeb, :live_view
 
+  alias LiveViewStudioWeb.Presence
+
+  @topic "users:video"
+
   def mount(_params, _session, socket) do
+    %{current_user: current_user} = socket.assigns
+
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(LiveViewStudio.PubSub, @topic)
+
+      {:ok, _} =
+        Presence.track(self(), @topic, current_user.id, %{
+          username: current_user.email |> String.split("@") |> hd(),
+          is_playing: false
+        })
+    end
+
+    presences = Presence.list(@topic)
+
     socket =
       socket
       |> assign(:is_playing, false)
+      |> assign(:presences, map_presences(presences))
 
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
+    <pre>
+      <%#= inspect @diff, pretty: true %>
+    </pre>
     <div id="presence">
       <div class="users">
         <h2>Who's Here?</h2>
-        <ul></ul>
+        <ul>
+          <li :for={{_user_id, meta} <- @presences}>
+            <span class="status">
+              <%= if meta.is_playing do %>
+                <.icon name="hero-play-circle-solid" />
+              <% else %>
+                <.icon name="hero-pause-circle-solid" />
+              <% end %>
+            </span>
+            <span class="username"><%= meta.username %></span>
+          </li>
+        </ul>
       </div>
       <div class="video" phx-click="toggle-playing">
         <%= if @is_playing do %>
@@ -29,6 +62,42 @@ defmodule LiveViewStudioWeb.PresenceLive do
 
   def handle_event("toggle-playing", _, socket) do
     socket = update(socket, :is_playing, fn playing -> !playing end)
+
+    %{current_user: current_user} = socket.assigns
+
+    %{metas: [meta | _]} = Presence.get_by_key(@topic, current_user.id)
+
+    new_meta = %{meta | is_playing: socket.assigns.is_playing}
+
+    # broadcasts presence_diff event
+    Presence.update(self(), @topic, current_user.id, new_meta)
+
     {:noreply, socket}
+  end
+
+  def handle_info(%{event: "presence_diff", payload: diff}, socket) do
+    socket =
+      socket
+      |> remove_presences(diff.leaves)
+      |> add_presences(diff.joins)
+
+    {:noreply, socket}
+  end
+
+  defp remove_presences(socket, leaves) do
+    user_ids = Enum.map(leaves, fn {user_id, _} -> user_id end)
+
+    presences = Map.drop(socket.assigns.presences, user_ids)
+
+    assign(socket, :presences, presences)
+  end
+
+  defp add_presences(socket, joins) do
+    presences = Map.merge(socket.assigns.presences, map_presences(joins))
+    assign(socket, :presences, presences)
+  end
+
+  defp map_presences(presences) do
+    Enum.into(presences, %{}, fn {user_id, %{metas: [meta | _]}} -> {user_id, meta} end)
   end
 end
