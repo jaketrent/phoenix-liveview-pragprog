69203f996ee4956c483a3f0561b34aee259ea472
Jake Trent <jake.trent@angel.com>
2024-02-27 16:42:58 -0700

    mod27: file upload to app server

--- lib/live_view_studio_web/live/desks_live.ex
+++ lib/live_view_studio_web/live/desks_live.ex
@@ -35,7 +35,22 @@ defmodule LiveViewStudioWeb.DesksLive do
     {:noreply, cancel_upload(socket, :photos, ref)}
   end
 
+  # files are uploaded to temp dir before this is called by form by phx-submit
   def handle_event("save", %{"desk" => params}, socket) do
+    photo_locations =
+      consume_uploaded_entries(socket, :photos, fn meta, entry ->
+        dest =
+          Path.join(["priv", "static", "uploads", "#{entry.uuid}-#{entry.client_name}"])
+
+        File.cp!(meta.path, dest)
+
+        url_path = static_path(socket, "/uploads/#{Path.basename(dest)}")
+
+        {:ok, url_path}
+      end)
+
+    params = Map.put(params, "photo_locations", photo_locations)
+
     case Desks.create_desk(params) do
       {:ok, _desk} ->
         changeset = Desks.change_desk(%Desk{})
